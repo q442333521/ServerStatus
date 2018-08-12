522f4eaa72c3da0f8b345abe914020f1a970e500
91yun <admin@91yun.org>
2018-08-12 17:03:49 +0800

    fix bugs

--- init.sh
+++ init.sh
@@ -6,8 +6,8 @@ then
 		PASSWORD="91yun"
 	fi
 	mv -f /ServerStatus/default.conf /etc/nginx/conf.d/default.conf
-	printf "${USERNAME}:$(openssl passwd -crypt ${PASSWORD})\n" >> /root/htpasswd
-	chmod 777 /root/htpasswd
+	printf "${USERNAME}:$(openssl passwd -crypt ${PASSWORD})\n" >> /ServerStatus/htpasswd
+	chmod 777 /ServerStatus/htpasswd
 fi
 
 nohup /etc/init.d/nginx start && /ServerStatus/server/sergate --config=/ServerStatus/server/config.json --port=3561 --web-dir=/usr/share/nginx/html
