e1c433cbed72d411328d319fe8d95181857cf1fe
91yun <admin@91yun.org>
2018-08-12 16:33:15 +0800

    fix bug

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ then
 	fi
 	mv -f /ServerStatus/default.conf /etc/nginx/conf.d/default.conf
 	printf "${USERNAME}:$(openssl passwd -crypt ${PASSWORD})\n" >> /root/htpasswd
+	chmod 777 /root/htpasswd
 fi
 
-/etc/init.d/nginx start
-nohup /ServerStatus/server/sergate --config=/ServerStatus/server/config.json --port=3561 --web-dir=/usr/share/nginx/html &
+nohup /etc/init.d/nginx start && /ServerStatus/server/sergate --config=/ServerStatus/server/config.json --port=3561 --web-dir=/usr/share/nginx/html
